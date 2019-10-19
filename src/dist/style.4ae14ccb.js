563d410f83782724397cd0b24b87ad8ad3fe8300
michalkruk <michal.k.kruk@gmail.com>
2019-10-19 20:32:13 +0200

    fix typo/links

--- src/dist/style.4ae14ccb.js
+++ src/dist/style.4ae14ccb.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55037" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56175" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
